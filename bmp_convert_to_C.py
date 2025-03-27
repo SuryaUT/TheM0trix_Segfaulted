31ed1797b5f4be4d21ca81b515399be41eedc8a3
SuryaUT <suryab@utexas.edu>
2025-03-27 13:49:31 -0500

    Adjusted printed c array format

--- bmp_convert_to_C.py
+++ bmp_convert_to_C.py
@@ -42,5 +42,4 @@ def bmp_to_c_array_direct_color_scaled(bmp_file_path):
 if __name__ == "__main__":
     input_image_path = input("Enter the path to the 32x32 image file (.bmp or .png): ")
     c_array_output = bmp_to_c_array_direct_color_scaled(input_image_path)
-    print("\nconst uint16_t myTexture[32 * 32] = ")
-    print(c_array_output)
+    print("\nconst uint16_t myTexture[32 * 32] = " + c_array_output)
