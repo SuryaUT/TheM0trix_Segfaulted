e2f8a9d772a025acb23a39412027d661dc4913df
Surya Balaji <suryab@utexas.edu>
2025-04-04 01:36:59 -0500

    Fixed inventory game logic bugs

--- Inventory.c
+++ Inventory.c
@@ -1,16 +1,29 @@
 #include "Graphics.h"
 #include "Inventory.h"
 
+uint8_t inInventory(Inventory* invent, Item* item){
+    for(int i = 0; i < invent->size; i++){
+        if(invent->items[i] == item){
+            return 1;
+        }
+    }
+    return 0;
+}
+
 uint8_t Inventory_add(Inventory* invent, Item* item){
     if (item->type == AMMO_SMALL) return AMMO_SMALL; // codes for ammo
     if (item->type == AMMO_BIG) return AMMO_BIG;
     if (invent->size == invent->cap) return 0;
-    item->invent_sprite.scale = 1;
-    invent->items[invent->size] = item;
-    invent->items[invent->size]->invent_sprite.x = 8 + (invent->size*16);
-    invent->items[invent->size]->invent_sprite.y = SCREEN_HEIGHT-1;
-    invent->size++;
-    return 1;
+    if(!inInventory(invent, item)){ //only adds item if not in inventory
+        item->invent_sprite.scale = 1;
+        invent->items[invent->size] = item;
+        invent->items[invent->size]->ammo = invent->items[invent->size]->mag_ammo;
+        invent->items[invent->size]->invent_sprite.x = 8 + (invent->size*16);
+        invent->items[invent->size]->invent_sprite.y = SCREEN_HEIGHT-1;
+        invent->size++;
+        return 1;
+    }
+    return 0;
 }
 
 void Inventory_replace(Inventory* invent, Item* item){
@@ -18,10 +31,13 @@ void Inventory_replace(Inventory* invent, Item* item){
 }
 
 void Inventory_removeCurrent(Inventory* invent){
-    for (int i = invent->index; i < invent->size; i++){
-        invent->items[i] = invent->items[i+1];
+    if(invent->index < (invent->size-1)){ //shift only if not removing last
+        for (int i = invent->index; i < invent->size - 1; i++){
+        invent->items[i+1]->invent_sprite.x = invent->items[i]->invent_sprite.x; //shift next item left in inventory
+        invent->items[i] = invent->items[i+1]; //replace removed elem with next elem
+        }
     }
-    invent->index--;
+    if(invent->index > 0) invent->index--; //dont dec if index is 0 to avoid hardfault
     invent->size--;
 }
 
